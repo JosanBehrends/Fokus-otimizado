58651f616c4af3c265a8029bc31c4b1b290c1339
Josan Behrends <josanbehrends@gmail.com>
2023-12-27 19:21:46 -0300

    Seleção de tarefas completa

--- script-crud.js
+++ script-crud.js
@@ -3,6 +3,8 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 const ulTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
+let tarefaSelecionada = null;
 
 function atualizarTarefas () {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -46,6 +48,21 @@ function criarElementoTarefa(tarefa) {
     li.append(paragrafo);
     li.append(botao);
     botao.append(imagemBotao);
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active').forEach(elemento => {
+            elemento.classList.remove('app__section-task-list-item-active')
+        });
+
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = '';
+            tarefaSelecionada = null;
+            return
+        }
+        tarefaSelecionada = tarefa;
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+        li.classList.add('app__section-task-list-item-active');
+    }
 
     return li
 }
